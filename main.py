0701aafcd1044acc56c9b26ba493498ed2260c2a
狐日泽 <Sun_Set1102@outlook.com>
2022-09-07 14:55:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ import os
 
 #check_update()
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)
+# if __name__ == "__main__":
+#     app.run(debug=True, host='0.0.0.0', port=80)
 
 title_console = Console(color_system="windows")
 start_console = Console(color_system="windows")
