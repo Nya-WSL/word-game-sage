2f9687f32579e4ea1c1fc77a3cbf816f96d244ac
狐日泽 <shdocter@nya-wsl.com>
2023-07-31 21:25:36 +0800

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# word-game-sage
-[windows安装示例](/pyinstaller.txt)
--- src/linux/update.py
+++
@@ -1,9 +0,0 @@
-import zipfile
-import urllib.request
-from urllib import request
-from configparser import ConfigParser
-
-def check_update():
-    LocalConfig = ConfigParser()
-    LocalConfig.read('version.ini')
-    File = LocalConfig.get('params', 'version')
